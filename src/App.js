336e10e15f4df03c650b46e68672c1a819e6de4f
github username <github email address>
2019-01-28 15:06:11 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import CatComponent from './CatComponent.js';
-import { EinsteinQuoteComponent } from './EinsteinQuoteComponent';
+import {EinsteinQuoteComponent} from './EinsteinQuoteComponent';
 import MouseComponent from './MouseComponent';
+// import rippleStartStatus from './hoverEffect';
+// import isIE11 from './hoverEffect';
+// import animationFrame from './hoverEffect';
 
 class App extends Component {
-	render() {
-		// your code in the return statement below!
-		return (
-			<div className="App">
-			</div>
-		);
-	}
+  render() {
+    // your code in the return statement below!
+    return ( <
+      div className = "App" >
+      <
+      CatComponent / >
+      <
+      EinsteinQuoteComponent / >
+      <
+      MouseComponent / >
+      <
+      /div>
+    );
+  }
 }
 
 export default App;
